d6035e557e0450a0acd47d0c43cf477c6879906a
iHoerth <hoerth.ignacio@gmail.com>
2022-08-24 18:23:33 -0300

    segundo comit

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+  return (
+    <div className='NavBar'>
+      <img className='logo' src='pokeball.png' alt="logo" />
+      <div className='NavBar_items'>INICIO</div>
+      <div className='NavBar_items'>SETTINGS</div>
+      <div className='NavBar_items'>LOG IN</div>
+    </div>
+  )
+}
+
+export default NavBar;

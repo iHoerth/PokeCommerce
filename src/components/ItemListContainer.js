0b53dfe67eb1f754752942caf22c44beefa11418
iHoerth <hoerth.ignacio@gmail.com>
2022-09-01 23:45:49 -0300

    FETCH2

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -8,9 +8,12 @@ const ItemListContainer = () => {
 
   async function fetchPokemon(idStart,idEnd){
     const allPokemon = [];
+    // idStart es el primer pokemon que quiero que traiga, idEnd el ultimo. ----> fetchPokemon(1,150) me va a traer los primeros 150 pokemons.
     for(let i = idStart; i <= idEnd; i++){
+      //Hago el fetch con la logica vista en clase
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
       const data = await response.json();
+      // Lo de aca abajo, es para tener mas facil acceso a los tipos del pokemon mas adelante.
       const pokemonTypes = getPokemonTypes(await data);
       data.types = pokemonTypes;
       allPokemon.push(data);
@@ -31,7 +34,7 @@ const ItemListContainer = () => {
       try{
         const pokeList = await fetchPokemon(1,150);
         setPokemons(pokeList);
-        console.log(pokeList);
+        console.log(pokeList); // voy a dejar este log porque me sirve ver la estructura del objeto pokemon para seguir trabajando.
       } catch (error) {
         console.log('hubo un error')
       }

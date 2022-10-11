4edb2f3231d3f53710bcbeb955ae6ef831434126
iHoerth <hoerth.ignacio@gmail.com>
2022-10-11 05:34:37 -0300

    Final 2

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -33,7 +33,7 @@ return (
               <div>Quantity: {ele.quantity}</div>
               <div>${ele.cost * ele.quantity}</div>
               <img src={ele.img} alt={ele.nombre+' img'} />
-              <Counter product={ele.poke} />
+              <Counter product={ele.poke} style={{position:'relative'}}/>
             </div>
        
           )
